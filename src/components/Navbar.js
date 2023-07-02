87652e934292b0dc35e732d4cac65e1d232b050f
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-07-02 11:41:31 -0400

    benefits section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
     return (
       <nav className="navbar">
         <Link className='navnar-link' to='/'><img className='logo' src={logo} alt='logo'/></Link>
+        <Link className='navbar-link' to="subscriptions" smooth={true} duration={500}>Subscriptions</Link>
         <Link className='navbar-link' to="about" smooth={true} duration={500}>About</Link>
-        <Link className='navbar-link' to="projects" smooth={true} duration={500}>Projects</Link>
         <Link className='navbar-link' to="contact" smooth={true} duration={500}>Contact</Link>
       </nav>
     );
