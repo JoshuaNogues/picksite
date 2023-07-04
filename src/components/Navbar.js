88dde90df6aef2527d65067944f7aca2f22916dc
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-07-04 16:05:01 -0400

    front end netlify almost done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ import logo from '../images/logo.png'
 const Navbar = () => {
     return (
       <nav className="navbar">
-        <Link className='navnar-link' to='/'><img className='logo' src={logo} alt='logo'/></Link>
-        <Link className='navbar-link' to="subscriptions" smooth={true} duration={500}>Subscriptions</Link>
+        <Link className='navbar-link' to='/'><img className='logo' src={logo} alt='logo'/></Link>
+        <Link className='navbar-link' to="plans" smooth={true} duration={500}>Subscriptions</Link>
         <Link className='navbar-link' to="about" smooth={true} duration={500}>About</Link>
         <Link className='navbar-link' to="contact" smooth={true} duration={500}>Contact</Link>
       </nav>
