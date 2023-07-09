7ff4acba631fac090115a0d6c8d04189e88b4bc2
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-07-09 10:00:49 -0400

    fixed number in contact

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <Link className='navbar-link' to='/'><img className='logo' src={logo} alt='logo'/></Link>
         <Link className='navbar-link' to="plans" smooth={true} duration={500}>Subscriptions</Link>
         <Link className='navbar-link' to="about" smooth={true} duration={500}>About</Link>
-        <a className='navbar-link' href="sms:+13053162533">Contact Us</a>
+        <a className='navbar-link' href="sms:+17863162533">Contact Us</a>
       </nav>
     );
   }
